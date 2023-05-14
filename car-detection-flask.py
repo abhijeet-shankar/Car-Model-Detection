5d4d089dfc8060bfa44f09032abe5c3cb1891aed
AbhijeetShankar <abhijeetshankar98@gmail.com>
2023-05-15 00:12:10 +0530

    minor commit

--- car-detection-flask.py
+++ car-detection-flask.py
@@ -1,6 +1,8 @@
 import flask
 from flask import Flask,render_template, request, send_file
 import cardetect
+from tensorflow.keras.preprocessing import image
+import os
 
 app= Flask(__name__,template_folder='meow')
 
@@ -12,12 +14,54 @@ def index():
 def home():
     return render_template('index.html')
 
+
+
+
 @app.route('/cardetect', methods=['GET','POST'])
 def cardetect():
-    img1= request.files['file-upload']
-    b=cardetect.detect(img1)
-    return render_template('prediction.html',b=b)
+    return render_template('prediction.html')
 
+
+
+@app.route('/detect', methods=['GET','POST'])
+def detect():
+    # if request.method==['POST']:
+    print(request.files)
+    img1= request.files['file']
+    img1=img1.filename
+    img=image.load_img(img1,target_size=(224,224))
+    x=image.img_to_array(img)
+    x=x/255
+    x=np.expand_dims(x,axis=0)
+    img_data=preprocess_input(x)
+    model.predict(img_data)
+
+    a=np.argmax(model.predict(img_data), axis=1)
+    b=int(a)
+    x=""
+    if (b==0):
+        x=x+"Audi"
+    elif (b==1):
+        x=x+"Bmw"    
+    elif (b==2):
+        x=x+"Bugatti"
+    elif(b==3):
+        x=x+"Ferrari"
+    elif(b==4):
+        x=x+"Maserati"
+    elif(b==5):
+        x=x+"Mercedes"
+    elif(b==6):
+        x=x+"RollsRoyce"
+    elif(b==7):
+        x=x+"Tesla"
+    elif(b==8):
+        x=x+"Toyota"
+    elif(b==9):
+        x=x+"Volkswagen"
+    # else:
+    #     return render_template('prediction.html',x=x)   
+    return render_template('prediction.html',x=x)    
 
 
 if __name__ == '__main__':
